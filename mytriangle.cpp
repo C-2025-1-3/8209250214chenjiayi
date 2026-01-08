28959e5b9fd5340b665db05133f88b587dcc506e
lighting36 <3092813294@qq.com>
2026-01-08 19:48:44 +0800

    Delete mytriangle.cpp

--- mytriangle.cpp
+++
@@ -1,8 +0,0 @@
-#include "mytriangle.h"
-bool is_valid(double a, double b, double c) {
-    return (a + b > c) && (a + c > b) && (b + c > a);
-}
-double area(double a, double b, double c) {
-    double s = (a + b + c) / 2.0;
-    return sqrt(s * (s - a) * (s - b) * (s - c));
-}
